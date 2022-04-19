33ee758a71f6be6df931190f85d8b54a3f136fb1
marsianca <xmarsiancax@mail.ru>
2022-04-19 11:33:37 +0300

    добавила images to AboutMeContent.tsx

--- src/components/about-me/AboutMePage.tsx
+++ src/components/about-me/AboutMePage.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import AboutMeContent from "./about-me-content/AboutMeContent";
 import Header from "../header/Header";
+import {Container} from "react-bootstrap";
+import "./about-me.scss"
 
 type AboutMeProps = {
     label:string
 }
 const AboutMe: React.FC<AboutMeProps> = (props): React.ReactElement => {
     return (
-        <div>
+        <div className={"about-me-wr"}>
             <Header label={props.label}/>
-            <AboutMeContent/>
+            <Container fluid className={"about-me-container"}>
+                <AboutMeContent/>
+            </Container>
         </div>
     )
 }
--- src/components/about-me/about-me-content/AboutMeContent.tsx
+++ src/components/about-me/about-me-content/AboutMeContent.tsx
@@ -1,8 +1,46 @@
+import React from "react";
+import {Col, Container, Row} from "react-bootstrap";
+import "./about-me-content.scss"
+import books from "../../../img/books.png"
+import bikeLogo from "../../../img/bikeLogo.png"
+import volleyball from "../../../img/volleyball.png"
+
 const AboutMeContent: React.FC = (): React.ReactElement => {
     return (
-        <div>
-            AboutMeContent
-        </div>
+        <Container className={"about-me-content"}>
+            <Container fluid>
+                AboutMeContent.
+            </Container>
+            <Container fluid>
+                <Row>
+                    <Col className={"img-block"} xs={12} md={4}>
+                        <figure>
+                            <img src={books} alt="books"/>
+                            <figcaption>
+                                <div><b>Чтение художественной литературы</b></div>
+                            </figcaption>
+                        </figure>
+                    </Col>
+                    <Col className={"img-block"} xs={12} md={4}>
+                        <figure>
+                            <img src={bikeLogo} alt="bikeLogo"/>
+                            <figcaption>
+                                <div><b>Велопрогулки</b></div>
+                            </figcaption>
+                        </figure>
+                    </Col>
+                    <Col className={"img-block"} xs={12} md={4}>
+                        <figure>
+                            <img src={volleyball} alt="voleibol"/>
+                            <figcaption>
+                                <div><b>Волейбол</b></div>
+                            </figcaption>
+                        </figure>
+                    </Col>
+                </Row>
+            </Container>
+
+        </Container>
     )
 }
 export default AboutMeContent
