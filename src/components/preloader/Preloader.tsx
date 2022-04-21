fb4050145d036b28374af7923216742457887844
marsianca <xmarsiancax@mail.ru>
2022-04-21 11:07:21 +0300

    поменяла лоадер на Бутстраповский

--- src/components/preloader/Preloader.tsx
+++ src/components/preloader/Preloader.tsx
@@ -1,10 +1,10 @@
-import preloader from "../../img/preloader.gif"
 import "./preloader.scss"
+import {Spinner} from "react-bootstrap";
 
 const Preloader = () => {
     return (
         <div className="preloader">
-            <img  src={preloader} alt="preloader"/>
+            <Spinner className={"spinner"} animation="grow" variant="info"  />
         </div>
     )
 }
--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,4 +1,4 @@
-import { call, put, all, takeEvery, takeLatest } from 'redux-saga/effects'
+import {call, put, all, takeEvery, delay} from 'redux-saga/effects'
 import {getImages} from "../../services/Api";
 import {actions} from "../reducers/img-reducer";
 import {AxiosResponse} from "axios";
@@ -7,7 +7,7 @@ import {Images} from "../../services/api-types";
 export function* fetchImg(): any {
     try {
         yield put(actions.setLoading(true));
-        // setTimeout(()=>{},500)
+        yield delay(2000);
         const images: AxiosResponse<Array<Images>> = yield call(getImages)
         yield put(actions.setLoading(false));
         yield put(actions.setImg(images.data));
@@ -20,7 +20,7 @@ function* watchFetchImg() {
     yield takeEvery('FETCH_IMAGES', fetchImg)
 }
 
-export function* mySaga(){
+export function* mySaga() {
     yield all([
         watchFetchImg(),
     ])
