f19490bc87a398feba9016c76ae261fe22f9505e
marsianca <xmarsiancax@mail.ru>
2022-05-05 10:44:29 +0300

    gh-pages

--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -2,5 +2,5 @@ import axios, {AxiosResponse} from "axios";
 import {Images} from "./api-types";
 
 export const getImages = (): Promise<AxiosResponse<Array<Images>>> => {
-    return axios.get("https://jsonplaceholder.typicode.com/photos?_start=0&_limit=24")
+    return axios.get("https://jsonplaceholder.typicode.com/posts")
 }
