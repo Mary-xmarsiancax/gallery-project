ee4fcce922278fd38d5fffa6dedb7f1413b59034
marsianca <xmarsiancax@mail.ru>
2022-05-06 11:09:59 +0300

    добавила <BrowserRouter basename={process.env.PUBLIC_URL}> to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter, useParams} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./redux/store";
 
@@ -13,9 +13,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <Provider store={store}>
-        <HashRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App/>
-        </HashRouter  >
+        </BrowserRouter  >
     </Provider>
 )
 ;
